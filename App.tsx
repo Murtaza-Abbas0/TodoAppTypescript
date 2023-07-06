6f65d15cddf249fe773290657d38bd893351c3b1
murtazarizvi0 <133651433+murtazarizvi0@users.noreply.github.com>
2023-07-06 20:14:02 +0545

    Integrated react navigations

--- App.tsx
+++ App.tsx
@@ -1,21 +1,26 @@
-import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const App = () => {
+function HomeScreen() {
   return (
-    <View style={styles.mainContainer} >
-      <Text>App</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
     </View>
-  )
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
-
-const styles = StyleSheet.create({
-  mainContainer: {
-    flex: 1,
-    backgroundColor: 'pink',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
